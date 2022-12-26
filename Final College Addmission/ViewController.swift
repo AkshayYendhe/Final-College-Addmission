0d1755e9af2ba7db2034e663683eef586604fce9
forloop <yendheakshayd@gmail.com>
2022-12-26 19:29:03 +0530

    Initial Commit

---
+++ Final College Addmission/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Final College Addmission
+//
+//  Created by Akshay Yendhe on 26/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
